f49f4bfcfd72903a83bc1f6515aea5167e1bdb43
elyzaj07 <elyzaj@uw.edu>
2026-09-02 23:39:04 -0700

    Update test_photometer.ino

--- Arduino/test_photometer.ino
+++ Arduino/test_photometer.ino
@@ -1,1 +1,15 @@
+int sensor_pin = A0;
+int brightness;
 
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Timestamp,brightness");
+}
+
+void loop() {
+  brightness = analogRead(sensor_pin);
+  Serial.print(millis());
+  Serial.print(",");
+  Serial.println(brightness);
+  delay(200);
+}
